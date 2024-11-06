65d22ee124f5c22c933a6108a89a4c81f490c0f6
notpatern <rodiersasha@gmail.com>
2024-11-06 03:08:23 +0100

    feat: anchor
    still need to implement update and render

---
+++ feur/src/Engine/GameObjects/Anchor.cpp
@@ -0,0 +1,8 @@
+#include "Anchor.hpp"
+
+Anchor::Anchor(float rubberLength, sf::Vector2i position, std::shared_ptr<sf::Texture> texture, float scale)
+: m_rubberLength{std::move(rubberLength)}, m_position{std::move(position)}, m_scale{std::move(scale)}, m_texture{texture}, m_bounds{static_cast<int>(texture->getSize().x * scale), static_cast<int>(texture->getSize().y * scale)}
+{
+    m_sprite->setTexture(*m_texture);
+    m_sprite->setScale(m_scale, m_scale);
+}
---
+++ feur/src/Engine/GameObjects/Anchor.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "SFML/Graphics/RenderTarget.hpp"
+#include "SFML/Graphics/Texture.hpp"
+#include "SFML/Graphics/Sprite.hpp"
+#include "SFML/System/Vector2.hpp"
+#include <memory>
+
+class Anchor {
+private:
+    const std::shared_ptr<sf::Texture> m_texture = std::make_shared<sf::Texture>();
+    std::unique_ptr<sf::Sprite> m_sprite = std::make_unique<sf::Sprite>();
+
+    sf::Vector2i m_position;
+    const sf::Vector2i m_bounds;
+    const float m_scale;
+    const float m_rubberLength;
+
+    bool m_isActive{false};
+
+public:
+    Anchor(float rubberLength, sf::Vector2i position,std::shared_ptr<sf::Texture> texture, float scale = 1);
+    ~Anchor();
+
+    [[nodiscard]] inline bool getState() const { return m_isActive; }
+    [[nodiscard]] inline bool getRubberLength() const { return m_rubberLength; }
+    [[nodiscard]] inline sf::Vector2i getPosition() const { return m_position; }
+    [[nodiscard]] inline sf::Vector2i getBounds() const { return m_bounds; }
+
+    inline void switchState() { m_isActive = !m_isActive; }
+    inline void setPosition(sf::Vector2i& position) { m_position = position; }
+
+    void update();
+    void render(sf::RenderTarget& target);
+};
